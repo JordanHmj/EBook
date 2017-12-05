47405a738b27bc1056ec3ba450331d39784357f1
HMJPC <153017010@qq.com>
2017-12-05 20:59:22 +0800

    success 1

--- EBook/src/EBook/BLL/Account.java
+++ EBook/src/EBook/BLL/Account.java
@@ -2,12 +2,15 @@ package EBook.BLL;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import EBook.Model.*;
+import EBook.DAO.*;
 
 /**
  * Servlet implementation class Account
@@ -30,7 +33,8 @@ public class Account extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html");
+		response.setHeader("Content-type", "text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		PrintWriter out=response.getWriter();
 		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
         out.println("<HTML>");
@@ -39,6 +43,23 @@ public class Account extends HttpServlet {
         out.print("    This is HMJ's ");
         out.print(this.getClass());
         out.println(", using the GET method");
+        
+        out.println("<br /><br />");
+        out.print("<table>");
+        BookDao bookDao=new BookDao();
+        List<Book> bookList=bookDao.getBooks();
+        
+        for(int i=0;i<bookList.size();i++)
+        {
+        	Book book=bookList.get(i);
+        	out.print("<tr>");
+        	out.print("<td>"+book.getNameCn()+"</td>");
+        	out.print("<td>"+book.getNameEn()+"</td>");
+        	out.print("<td>"+book.getScore()+"</td>");
+        	out.print("<td>"+book.getDownNum()+"</td>");
+        	out.print("</tr>");
+        }
+        out.println("  </table>");
         out.println("  </BODY>");
         out.println("</HTML>");
         out.flush();
