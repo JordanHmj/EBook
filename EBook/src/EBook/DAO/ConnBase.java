d44d31eaaaa5bb0930db494489eee4466e00ff42
JordanHmj <153017010@qq.com>
2018-02-08 13:04:13 +0800

    Delete ConnBase.java
    security

--- EBook/src/EBook/DAO/ConnBase.java
+++
@@ -1,9 +0,0 @@
-package EBook.DAO;
-
-public class ConnBase {
-	public static String url="jdbc:mysql://47.93.246.137:3306/EBookDB?characterEncoding=utf8&useSSL=false";
-	
-	public static String userName="root";
-	
-	public static String password = "JordanHmj@01";
-}
